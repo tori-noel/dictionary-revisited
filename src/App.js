e79419398a0efe3a19ee22acfbea42fd9838f710
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-08-23 13:47:20 -0400

    integrated dictionary api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import banner from "./banner.png";
 import "./App.css";
-
+import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
       <header>
-        <img src={banner} className="App-logo img-fluid" alt="logo" />
+        <img src={banner} className="App-logo img-fluid shadow" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  function search(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
