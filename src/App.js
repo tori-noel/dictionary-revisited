b849ff757f3f23764d89829d1e3eee5b5c2746fe
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-08-23 12:25:08 -0400

    created basic structure and added a logo in the header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import banner from "./banner.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={banner} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
